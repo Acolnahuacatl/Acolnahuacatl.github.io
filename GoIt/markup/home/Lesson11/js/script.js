f13263077593331fc523eff7d1f67016f4ea9076
Acolnahuacatl <Inkub2007@gmail.com>
2015-12-01 19:52:28 +0200

    lesson11

--- GoIt/markup/home/Lesson11/js/script.js
+++ GoIt/markup/home/Lesson11/js/script.js
@@ -8,19 +8,21 @@ var names = ['name'];
 var nameNumber = 1;
 
 for (var i = 0; i < 6; i++) {
-
-    names[i] = prompt('ВВедите ' + nameNumber++ + "-е имя:");
-    
+    names[i] = prompt('Введите ' + nameNumber++ + "-е имя:");
 }
 
 console.log(names);
 
 var enterUserName = prompt('Введите имя пользователя');
 
-for (var k = 0; k < 6; k++) {
+for(var k = 0, l = names.length; k < l; k++) {
     if (enterUserName == names[k]){
-        alert('Привет ' + names[k]);
-    } else if(k == 5) {
-        alert('Имя пользователя введео неверно!');
+        var hello = 'Привет ' + names[k];
     }
-} 
+}
+
+if (hello) {
+    alert(hello);
+} else {
+    alert('Имя пользователя введено нe верно!');
+}
