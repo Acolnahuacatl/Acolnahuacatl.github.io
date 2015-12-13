68510b51fe91205d89838950e519f18f1a58180b
Acolnahuacatl <Inkub2007@gmail.com>
2015-12-13 16:41:53 +0200

    update homework 4

--- GoIt/markup/home/Java03-04/js/script.js
+++ GoIt/markup/home/Java03-04/js/script.js
@@ -4,70 +4,171 @@
  * and open the template in the editor.
  */
 
+//var app = {
+//    genElement: function (params) {
+//        var element = document.createElement(params.tagName);
+//        if (params.inputType) {
+//            element.setAttribute('type', params.inputType);
+//            element.setAttribute('value', params.inputValue);
+//        }
+//        if (params.className) {
+//            element.className = params.className;
+//        }
+//        if (params.content) {
+//            element.innerHTML = params.content;
+//        }
+//        if (params.parent){
+//            params.parent.appendChild(element);
+//        }
+//        return element;
+//    },
+//    genQuestions: function(questions, ansvers) {
+//        
+//        var fildset = document.querySelector('fildset');
+//        
+//        var ul = app.genElements({
+//            tagName: 'ul',
+//            className: 'question-list',
+//            parent: fildset
+//        });
+//        for (var i = 0; i < questions; i++) {
+//                var li = this.genElements({
+//                tagName: 'li',
+//                content: (i + 1) + '.Вопрос №' + (i+1),
+//                className: 'java-ul',
+//                parent: ul
+//            });
+//            for (var j = 0; j < ansvers; j++) {
+//                var label = this.genElements({
+//                    tagName: 'label',
+//                    content: 'Вариант ответа №'+(j+1),
+//                    className: 'java-label',
+//                    parent: li
+//                });
+//                var checkbox = this.genElements({
+//                    tagName: 'input',
+//                    inputType: 'checkbox'
+//                });
+//                label.insertAdjacentElement('afterBegin', checkbox);
+//            }
+//        };
+//    }
+//};
+
+
+
 var app = {
-    genElements: function (params) {
+    genElement: function (params) {
         var element = document.createElement(params.tagName);
-        if (params.inputType) {
-            element.setAttribute('type', params.inputType);
-            element.setAttribute('value', params.inputValue);
-        }
         if (params.className) {
             element.className = params.className;
         }
         if (params.content) {
             element.innerHTML = params.content;
         }
+        if (params.attrs){
+        var attrs = new Object();
+            for (var key in params.attrs){
+                element.setAttribute(key, params.attrs[key]);
+            }
+        }
         if (params.parent){
             params.parent.appendChild(element);
         }
         return element;
     },
-    genQuestion: function(questions, ansvers) {
+    genQuestions: function(content) {
         
         var fildset = document.querySelector('fildset');
         
-        var ul = app.genElements({
+        var ul = this.genElement({
             tagName: 'ul',
             className: 'question-list',
             parent: fildset
         });
-        for (var i = 0; i < questions; i++) {
-                var li = this.genElements({
-                tagName: 'li',
-                content: (i + 1) + '.Вопрос №' + (i+1),
-                className: 'java-ul',
-                parent: ul
+            var li = this.genElement({
+            tagName: 'li',
+            className: 'java-ul',
+            parent: ul
+        });
+        li.innerHTML = content.question;
+        var ansv = [];
+        for (var i in content.ansv) {
+            var label = this.genElement({
+                tagName: 'label',
+                className: 'java-label',
+                parent: li
             });
-            for (var j = 0; j < ansvers; j++) {
-                var label = this.genElements({
-                    tagName: 'label',
-                    content: 'Вариант ответа №'+(j+1),
-                    className: 'java-label',
-                    parent: li
-                });
-                var checkbox = this.genElements({
-                    tagName: 'input',
-                    inputType: 'checkbox'
-                });
-                label.insertAdjacentElement('afterBegin', checkbox);
+        for (var k = 0; k < content.ansv.length; k++){
+            label.innerHTML = content.ansv[i];
+        }
+            var checkbox = this.genElement({
+                tagName: 'input',
+                attrs: {
+                    type: 'checkbox'
+                }
+            });
+            label.insertAdjacentElement('afterBegin', checkbox);
+        }
+    },
+    attachEvent: function (event){
+        var javaForm = this.genElement({
+            tagName: 'form',
+            className: 'wrapper',
+            parent: body
+        });
+        this.genElement({
+            tagName: 'button',
+            className: 'close-button',
+            parent: javaForm,
+            attrs: {
+                input: 'button',
+                onClick: 'closeMe()'
             }
+        });
+        function closeMe(){
+             javaForm = undefined;  
         };
+        var javaFildset = this.genElement({
+            tagName: 'fildset',
+            parent: javaForm
+        });
+        var javaLegend = this.genElement({
+            tagName: 'legend',
+            className: 'java-legend',
+            //content: 'Тест по программированию',
+            parent: javaFildset
+        });
+        javaLegend.innerHTML = event.name;
+        var results = this.genElement({
+            tagName: 'input',
+            className: 'java-submit',
+            parent: javaFildset,
+            attrs: {
+                type: 'button',
+                value: 'Провеить мои результаты'
+            }
+        });
+        javaFildset.insertAdjacentElement('afterEnd', results);
     }
 };
 
 var body = document.querySelector('body');
 
-var button = app.genElements({
-    tagName: 'input',
-    inputType: 'button',
-    className: 'javaStart',
-    inputValue: 'Touch Me!',
-    parent: body
-});
+var body = document.querySelector('body');
 
-button.setAttribute('onmouseover', 'overButton()');
-button.setAttribute('onClick', 'tuchMe()');
-button.setAttribute('onmouseout', 'haHa()');
+var button = app.genElement({
+    tagName: 'input',
+    className: 'javaStart',
+    parent: body,
+    attrs: {
+        value: 'Touc Me!',
+        type: 'button',
+        onmouseover: 'overButton()',
+        onClick: 'tuchMe()',
+        onmouseout: 'haHa()'
+    }
+});
 
 function overButton(){
     button.setAttribute('value', 'Are you sure?');
@@ -76,44 +177,69 @@ function haHa(){
     button.setAttribute('value', 'Tuch Me!');
 }
 
-    
 function tuchMe(){
-    var javaForm = app.genElements({
-        tagName: 'form',
-        className: 'wrapper',
-        parent: body
+    app.attachEvent({
+        name: 'Набор вопросов'
     });
-    var close = app.genElements({
-        tagName: 'button',
-        className: 'close-button',
-        parent: javaForm
+    app.genQuestions({
+        question: 'Где живут кролики?',
+        ansv: [
+            'В норах',
+            'В пещеах',
+            'В нарнии'
+        ]
     });
-    
-    close.setAttribute('onClick', 'closeMe()');
-    
-    function closeMe(){
-      javaForm = undefined;  
-    };
-    
-    var javaFildset = app.genElements({
-        tagName: 'fildset',
-        parent: javaForm
-    });
-    app.genElements({
-        tagName: 'legend',
-        className: 'java-legend',
-        content: 'Тест по программированию',
-        parent: javaFildset
-    });
-    
-    app.genQuestion(3, 3);
-    
-    app.genElements({
-        tagName: 'input',
-        inputType: 'Button',
-        inputValue: 'Провеить мои результаты',
-        className: 'java-submit',
-        parent: javaFildset
+    app.genQuestions({
+        question: 'Гномы это?',
+        ansv: [
+            'Эльфы, которые много болели',
+            'Маленький жадый наод, придуманный Толкиеном',
+            'Вообще без понятия'
+        ]
     });
 }
+
+//button.setAttribute('onmouseover', 'overButton()');
+//button.setAttribute('onClick', 'tuchMe()');
+//button.setAttribute('onmouseout', 'haHa()');
+
+//function tuchMe(){
+//    var javaForm = app.genElement({
+//        tagName: 'form',
+//        className: 'wrapper',
+//        parent: body
+//    });
+//    var close = app.genElement({
+//        tagName: 'button',
+//        className: 'close-button',
+//        parent: javaForm
+//    });
+//    
+//    close.setAttribute('onClick', 'closeMe()');
+//    
+//    function closeMe(){
+//      javaForm = undefined;  
+//    };
+//    
+//    var javaFildset = app.genElement({
+//        tagName: 'fildset',
+//        parent: javaForm
+//    });
+//    app.genElement({
+//        tagName: 'legend',
+//        className: 'java-legend',
+//        content: 'Тест по программированию',
+//        parent: javaFildset
+//    });
+//    
+//    app.genQuestions(3, 3);
+//    
+//    app.genElement({
+//        tagName: 'input',
+//        inputType: 'Button',
+//        inputValue: 'Провеить мои результаты',
+//        className: 'java-submit',
+//        parent: javaFildset
+//    });
+//}
 
