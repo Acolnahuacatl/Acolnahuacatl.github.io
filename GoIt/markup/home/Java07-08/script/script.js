588ce6be88739dadab57a0494b256c29de24ea8b
Acolnahuacatl <Inkub2007@gmail.com>
2016-01-07 17:22:46 +0200

    JS 07-08 in one

---
+++ GoIt/markup/home/Java07-08/script/script.js
@@ -0,0 +1,71 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$(function () {
+        var $link = $('.tab-link');
+    
+    
+    $link.on('click', function (e) {
+        var $hidden = $('.active');
+        var $links = $('.selected');
+        var $content = $('.cut p');
+        e.preventDefault();
+        
+        if (this.href.match('tab-2')) {
+        $content.eq(1).addClass('active-tab');
+        $content.eq(0).removeClass('active-tab');
+        $content.eq(2).removeClass('active-tab');
+        $content.eq(0).addClass('hidden');
+        $content.eq(2).addClass('hidden');
+        }
+        if (this.href.match('tab-1')) {
+        $content.eq(0).addClass('active-tab');
+        $content.eq(1).removeClass('active-tab');
+        $content.eq(2).removeClass('active-tab');
+        $content.eq(1).addClass('hidden');
+        $content.eq(2).addClass('hidden');
+        }
+        if (this.href.match('tab-3')) {
+        $content.eq(2).addClass('active-tab');
+        $content.eq(0).removeClass('active-tab');
+        $content.eq(1).removeClass('active-tab');
+        $content.eq(0).addClass('hidden');
+        $content.eq(1).addClass('hidden');
+        }
+            
+        $hidden.removeClass('active');
+        $links.removeClass('selected');
+        $(this).addClass('active');
+    });
+    
+    var $input = $('.input');
+    
+    $input.on('mouseover', function (e){
+        var $help = $(this).siblings('p'); 
+        e.preventDefault();
+        
+        $help.addClass('active-help');
+        $help.removeClass('hidden-help');
+        
+    });
+    $input.on('mouseout', function (e){
+        var $help = $(this).siblings('p'); 
+        e.preventDefault();
+        
+        $help.removeClass('active-help');
+        $help.addClass('hidden-help');
+    });
+    
+    var $showHelp = $('.button');
+    
+    $showHelp.on('click', function () {
+        var $helps = $('.fild-wrapper p');
+        
+        $helps.removeClass('hidden-help');
+        $helps.addClass('active-help');
+        
+    });
+});
